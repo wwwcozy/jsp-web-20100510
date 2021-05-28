430a2eafd52d4c4dbd9058f1e6104411d808744a
jean5694 <jeanjean5694@gmail.com>
2021-05-28 17:02:42 +0900

    exercise - ing

---
+++ src/CH14/bean/Employee.java
@@ -0,0 +1,27 @@
+package CH14.bean;
+
+public class Employee {
+	private int eid;
+	private String lastName;
+	private String firstName;
+	
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+}
+	
