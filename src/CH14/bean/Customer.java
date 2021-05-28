f7af11c8667dbf1c0158072f2b9d69326e2375f8
jean5694 <jeanjean5694@gmail.com>
2021-05-28 15:23:10 +0900

    ch14

---
+++ src/CH14/bean/Customer.java
@@ -0,0 +1,20 @@
+package CH14.bean;
+
+public class Customer {
+	private String name;
+	private String city;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+}
