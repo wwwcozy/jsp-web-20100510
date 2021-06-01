7871f9ea01a227fea3d81ed71765a51f08d9d559
jean5694 <jeanjean5694@gmail.com>
2021-06-01 18:54:22 +0900

    sql

---
+++ WebContent/WEB-INF/sql/DELETE01.sql
@@ -0,0 +1,24 @@
+SELECT * FROM Customers
+ORDER BY CustomerID;
+
+DELETE FROM Customers;
+
+DELETE FROM Customers
+WHERE CustomerID = 1;
+
+-- exercise 1
+SELECT * FROM Customers
+WHERE Country = 'Norway';
+
+
+DELETE FROM
+ Customers
+WHERE
+ Country = 'Norway';
+ 
+ 
+ -- exercise 2
+SELECT * FROM Customers;
+
+DELETE FROM Customers;
+
--- WebContent/WEB-INF/sql/UPDATE01.sql
+++ WebContent/WEB-INF/sql/UPDATE01.sql
@@ -1,15 +1,21 @@
-SELECT * FROM Customers;
+SELECT * FROM Customers
+WHERE CustomerID = 1;
+
+SELECT * FROM Employees
+WHERE EmployeeID = 2;
 
 UPDATE Customers
-SET CustomerName = '스파이더맨'; -- 모든 customerName이 변경됨
+SET CustomerName = '스파이더맨'; -- 모든 customerName column 값이 변경됨
 
 UPDATE Customers
 SET CustomerName = '아이언맨'
-WHERE CustomerID = 1;  -- where절이 생략되면 모든 절에 영향을 끼침 / 주의할 것
+WHERE CustomerID = 1;  
+-- where : 업데이트가 필요한 레코드 지정
+-- where절이 생략될 경우, 모든 테이블에 영향을 끼침 / 주의할 것
 
 UPDATE Customers
 SET CustomerName = 'thor',
 	ContactName = 'odinson',
     Address = 'Asgard'
 WHERE CustomerID = 1;
-
+-- 여러 column 값을 수정해야 하는 경우 (,) 쉼표로 구분해서 작성한다
