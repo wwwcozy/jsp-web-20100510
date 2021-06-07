fcecac0a75bad041b928cd7ec9db18bf656397fb
jean5694 <jeanjean5694@gmail.com>
2021-06-07 16:10:31 +0900

    subquery

---
+++ WebContent/WEB-INF/sql/SUBQUERY03.sql
@@ -0,0 +1,22 @@
+USE test;
+
+SELECT ProductName
+FROM Products
+WHERE ProductID = ANY
+  (SELECT ProductID
+  FROM OrderDetails
+  WHERE Quantity = 10);
+  
+SELECT ProductID
+FROM OrderDetails
+WHERE Quantity = 10;
+
+SELECT * FROM OrderDetails;
+
+-- ALL
+SELECT ProductName
+FROM Products
+WHERE ProductID = ALL
+  (SELECT ProductID
+  FROM OrderDetails
+  WHERE Quantity = 10);
