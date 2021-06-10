bcde89b147d93db46b62cb1d3c291aef23e59778
jean5694 <jeanjean5694@gmail.com>
2021-06-10 10:43:26 +0900

    having

---
+++ WebContent/WEB-INF/sql/GROUP03.sql
@@ -0,0 +1,19 @@
+USE test;
+
+SELECT Count(CustomerID), Country
+FROM Customers
+group by country
+HAVING COUNT(CustomerID) > 5; -- having 조건절
+
+SELECT Employees.LastName, COUNT(Orders.OrderID) AS NumberOfOrders
+FROM (Orders
+INNER JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID)
+GROUP BY LastName
+HAVING COUNT(Orders.OrderID) > 10;
+
+SELECT Employees.LastName, COUNT(Orders.OrderID) AS NumberOfOrders
+FROM Orders
+INNER JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+WHERE LastName = 'Davolio' OR LastName = 'Fuller'
+GROUP BY LastName
+HAVING COUNT(Orders.OrderID) > 25;
